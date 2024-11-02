9ad049f8f217f7e7bdeac1567a871d8cd0de7aa4
rajeshmajhi1 <rajeshmajhi1@gmail.com>
2024-11-02 05:06:14 +0000

    idx

--- lib/views/nowplaying.dart
+++ lib/views/nowplaying.dart
@@ -1,5 +1,4 @@
 import 'package:bookit/services/api.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
@@ -16,16 +15,37 @@ class _NowPlayingState extends ConsumerState<NowPlaying> {
     final json = Api().loadData();
     return ListView.builder(
         scrollDirection: Axis.horizontal,
-        itemCount: json.length,
-    
+        itemCount: json['movies'].length,
         shrinkWrap: true,
         itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Image.network(json['movies'][index]['image_url'],
-                  width: 150, height: 150),
-              Text(json['movies'][index]['title']),
-            ],
+          return Card(
+            clipBehavior: Clip.antiAlias, // Add rounded corners
+            child: Column(
+              crossAxisAlignment:
+                  CrossAxisAlignment.center, // Align items to the start
+              children: [
+                Image.network(json['movies'][index]['image_url'],
+                    width: 150,
+                    height: 200,
+                    fit: BoxFit.cover), // Adjust image size and fit
+                Padding(
+                  padding: const EdgeInsets.all(8.0), // Add padding
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        json['movies'][index]['title'],
+                        style: const TextStyle(
+                            fontWeight: FontWeight.bold), // Make title bold
+                      ),
+                      // Add more movie details here if available in your JSON data
+                      // e.g., Text('Genre: ${json['movies'][index]['genre']}'),
+                      //       Text('Rating: ${json['movies'][index]['rating']}'),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           );
         });
   }
