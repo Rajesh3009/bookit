378abed51658d5102274506394d14384278dd152
Rajesh3009 <Rajesh3009@protonmail.com>
2024-12-09 19:56:41 +0530

    Enhances wallet functionality and user experience
    Adds wallet balance initialization from Firestore during user login
    Implements a top-up feature with payment options integrated into the wallet page
    
    Updates APK build settings and release artifact paths for better deployment
    Increments project version to 0.1.9+9
    
    Relates to user wallet management improvements

--- lib/providers/wallet_provider.dart
+++ lib/providers/wallet_provider.dart
@@ -1,10 +1,21 @@
-
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 class WalletNotifier extends StateNotifier<double> {
   WalletNotifier(super.state) {
-    state = 100.0;
+    _load();
+  }
+
+  _load() async {
+    state = await FirebaseFirestore.instance
+            .collection('users')
+            .doc(FirebaseAuth.instance.currentUser!.uid)
+            .get()
+            .then((doc) => state = doc.data()!['wallet']) ??
+        0;
   }
 }
+
 final walletProvider =
     StateNotifierProvider<WalletNotifier, double>((ref) => WalletNotifier(0.0));
